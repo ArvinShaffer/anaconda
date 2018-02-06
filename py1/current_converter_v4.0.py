6d846aa542e6b17f584b70a07d090d30a0a4c2b7
Your Name <you@example.com>
2018-02-06 11:24:12 +0800

    Lesson23 04_ Exchange Rate

---
+++ py1/current_converter_v4.0.py
@@ -0,0 +1,42 @@
+"""
+    Author:Arvin Shaffer
+    Function:Exchange rate
+    Version:1.0
+    Date:02/01/2018
+    2.0 New Function:According to the input to determine whether the RMB or the dollar, the corresponding conversion calculation
+    3.0 New Function:The program runs until the user chooses to quit!
+    4.0 New Function:Exchange rate conversion function into the function!!
+"""
+
+def convert_currency(im, er):
+    """
+    Exchange rate
+    :param im:the amount of money
+    :param er: exchange rate
+    :return:
+    """
+    out = im*er
+    return out
+
+#exchange rate
+usd_vs_rmb = 6.77
+
+currency_str_value = input('Please enter the amount of currency with unit(Exit the program, please enter(Q))：')
+
+#get the unit
+unit = currency_str_value[-3:]
+
+if unit == 'CNY':
+    exchange_rate = 1 / usd_vs_rmb
+elif unit == "USD":
+    exchange_rate = usd_vs_rmb
+else:
+    exchange_rate = -1
+
+if exchange_rate != -1:
+    in_money = eval(currency_str_value[:-3])
+    #call function convert_currency()
+    out_money = convert_currency(in_money, exchange_rate)
+    print('Converted amount:', out_money)
+else:
+    print('This currency is not supported!')
