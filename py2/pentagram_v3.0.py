ba474f288ad0eb0694ab8ba75a9b266e52e362c0
Arvin Shaffer <jianzhimo@sina.cn>
2018-02-14 17:21:26 +0800

    Lesson 31 03_ Fractal tree rendering

---
+++ py2/pentagram_v3.0.py
@@ -0,0 +1,50 @@
+"""
+    Author:Arvin Shaffer
+    Function:Draw a five-pointed star!
+    Version:3.0
+    Date:02/14/2018
+    3.0 New Function:Use iteration function draw plots of different sizes
+"""
+import turtle
+
+def draw_pentagram(size):
+    """"
+        Draw a five-pointed star!
+    """
+    count = 1
+    while count <= 5:
+        turtle.forward(size)
+        turtle.right(144)
+        count += 1
+
+def draw_recursive_pentagram(size):
+    """
+    Use iteration to draw a five-pointed star
+    :param size:
+    :return:
+    """
+    count = 1
+    while count <= 5:
+        turtle.forward(size)
+        turtle.right(144)
+        count += 1
+
+    #update the value of size
+    size += 10
+    if size <=100 :
+        draw_recursive_pentagram(size)
+
+def main():
+    turtle.penup()
+    turtle.bk(200)
+    turtle.pendown()
+    turtle.pensize(2)
+    turtle.pencolor('red')
+
+    size = 50
+    draw_recursive_pentagram(size)
+
+    turtle.exitonclick()
+
+if __name__ == "__main__" :
+    main()
