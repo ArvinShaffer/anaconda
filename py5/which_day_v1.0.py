607bd73c3a14df39280700ce7901bd0f3536911b
Your Name <you@example.com>
2018-02-26 10:34:19 +0800

    Lesson 53 01_ Which day?

---
+++ py5/which_day_v1.0.py
@@ -0,0 +1,29 @@
+"""
+    Author:Arvin Shaffer
+    Function:Which day ?
+    Version:1.0
+    Date:02/26/2018
+    Function:Enter a certain date of a year to determine the day is the first few days of the year
+"""
+from datetime import datetime
+
+def main():
+    input_data_str = input('Please enter the date(yyyy/mm/dd):')
+    input_data = datetime.strptime(input_data_str, '%Y/%m/%d')
+    #print(input_data)
+    year = input_data.year
+    month = input_data.month
+    day = input_data.day
+
+    #Calculate
+    days_in_month_tup = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    days = sum(days_in_month_tup[:month-1]) + day
+
+    #To determine whether the leap year
+    if (year%400 == 0) or ((year%4==0) and (year%100!=0)):
+        if month > 2:
+            days += 1
+    print('This is the {}th day'.format(days))
+
+if __name__ == '__main__':
+    main()
